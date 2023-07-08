0291162e273d1e9941b7733fcf78cf011a126062
IKaRos <w71800@gmail.com>
2023-07-08 11:55:09 +0800

    fixed the bug of overflow

--- week1/dist/script.js
+++ week1/dist/script.js
@@ -1,7 +1,19 @@
 const hamberger = document.querySelector(".hamberger")
 const sidebar = document.querySelector("#sidebar")
+let appear = false
 
 hamberger.addEventListener('click', ()=>{
+  appear = !appear
   hamberger.classList.toggle("active")
-  sidebar.classList.toggle("in")
+  if(appear){
+    sidebar.classList.toggle("appear")
+    setTimeout(() => {
+      sidebar.classList.toggle("in")
+    }, 100)
+  }else{
+    sidebar.classList.toggle("in")
+    setTimeout(() => {
+      sidebar.classList.toggle("appear")
+    }, 500)
+  }
 })
