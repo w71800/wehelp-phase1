8bedb403f8f7b48c6396238437635ee8953cb3bf
IKaRos <w71800@gmail.com>
2023-07-10 15:24:21 +0800

    create files

---
+++ week2/tasks_py/task1.py
@@ -0,0 +1,10 @@
+# def find_and_print(messages):
+# # write down your judgment rules in comments
+# # your code here, based on your own rules
+# find_and_print({
+# "Bob":"My name is Bob. I'm 18 years old.", "Mary":"Hello, glad to meet you.",
+# "Copper":"I'm a college student. Nice to meet you.", "Leslie":"I am of legal age in Taiwan.",
+# "Vivian":"I will vote for Donald Trump next week", "Jenny":"Good morning."
+# })
+
+x = 3
