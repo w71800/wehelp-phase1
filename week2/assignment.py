514828d94cd057b6123f8b15cc8c6b9c5220978b
IKaRos <w71800@gmail.com>
2023-07-10 15:53:57 +0800

    task1.py complete

--- week2/assignment.py
+++ week2/assignment.py
@@ -2,4 +2,4 @@ import sys
 sys.path.append("tasks_py")
 
 import task1 as t1
-print(task1.x)
+t1.result
--- week2/tasks_py/task1.py
+++ week2/tasks_py/task1.py
@@ -1,10 +1,29 @@
-# def find_and_print(messages):
-# # write down your judgment rules in comments
-# # your code here, based on your own rules
-# find_and_print({
-# "Bob":"My name is Bob. I'm 18 years old.", "Mary":"Hello, glad to meet you.",
-# "Copper":"I'm a college student. Nice to meet you.", "Leslie":"I am of legal age in Taiwan.",
-# "Vivian":"I will vote for Donald Trump next week", "Jenny":"Good morning."
-# })
+def find_and_print(messages):
+  ## 目標：做出 [{"name": "...", "message": "...} ] 這樣的結構
+  keywords = ["18", "college", "legal age", "will vote"]
+  msgList = []
 
-x = 3
+  for item in messages.items():
+    containerDict = {
+      "name": None,
+      "msg": None
+    }
+    containerDict["name"] = item[0]
+    containerDict["msg"] = item[1] 
+    msgList.append(containerDict)
+  
+  for item in msgList:
+    for word in keywords:
+      if word in item["msg"]:
+        print(item["name"])
+    
+
+# keywords 18, college, legal age, will vote
+result = find_and_print({
+  "Bob": "My name is Bob. I'm 18 years old.", 
+  "Mary": "Hello, glad to meet you.",
+  "Copper": "I'm a college student. Nice to meet you.",
+  "Leslie": "I am of legal age in Taiwan.",
+  "Vivian": "I will vote for Donald Trump next week",
+  "Jenny": "Good morning."
+})
