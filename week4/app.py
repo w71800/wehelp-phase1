b37ddeabf78719ef78cd6b24ea6567b68077f435
IKaRos <w71800@gmail.com>
2023-07-25 15:41:53 +0800

    complete basic assignment of Week4

--- week4/app.py
+++ week4/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 @app.route("/")
 def home():
@@ -8,9 +9,44 @@ def home():
 
 @app.route("/signin", methods=["POST"]) 
 def signIn():
-  return "來到登入頁面了"
+  userId = request.form.get("id")
+  userpwd = request.form.get("password")
+
+  print(userId, userpwd)
+  if userId and userpwd == "test":
+    session["SIGNED-IN"] = True
+    return redirect("/member")
+  elif userId == "" or userpwd == "" :
+    errorMsg = "Please enter username and password"
+    return redirect(url_for("error", message = errorMsg))
+  else:
+    errorMsg = "Username or password is not correct"
+    return redirect(url_for("error", message = errorMsg))
+
+@app.route("/member") 
+def member():
+  SIGNED_IN = session.get("SIGNED-IN")
+
+  print(SIGNED_IN)
+  if SIGNED_IN:
+    return render_template("member.html")
+  else:
+    return redirect("/")
+
+@app.route("/signout") 
+def signout():
+  SIGNED_IN = session.get("SIGNED-IN")
+  if SIGNED_IN:
+    session.pop("SIGNED-IN")
+    return redirect("/")
+
+@app.route("/error") 
+def error():
+  errorMsg = request.args.get("message")
+  return render_template("error.html", message = errorMsg)
+
 
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(port=3000, debug=True)
