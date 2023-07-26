b6c05dedfb26c30084e1f54eea3c8931721f1bc7
IKaRos <w71800@gmail.com>
2023-07-26 17:54:01 +0800

    optimized

--- week4/app.py
+++ week4/app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "secret"
 
-app.static_folder = "static"
 
 @app.route("/")
 def home():
