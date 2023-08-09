875765f92228b470fcbf387e75d6f9fa39722419
IKaRos <w71800@gmail.com>
2023-08-09 16:09:35 +0800

    complete req.2

--- week6/app.py
+++ week6/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from db_manufactor import cursor
+from flask import Flask, render_template, request, redirect, url_for
+from db_manufactor import db, cursor
 
 app = Flask(__name__)
 
@@ -23,8 +23,32 @@ def signin():
   user_id = request.form.get("user_id")
   user_password = request.form.get("user_password")
   # 先比對資料庫中有無此 id，有的話對對看密碼，沒有的話就要求申請帳號
-  # 帳號密碼都對之後，帶著 name 重新導向至 /member
+  # 帳號密碼都對之後，重新導向至 /member，並且用 session 來存取 name
   return render_template("member.html")
+
+# 處理註冊需求
+@app.route("/signup", methods=["POST"])
+def signup():
+  user_username = request.form.get("id")
+  sql = "SELECT username from member"
+  cursor.execute(sql)
+  signed_members = cursor.fetchall()
+  signed_members = [ item[0] for item in signed_members ]
+  
+  # 已經被註冊的狀況
+  if user_username in signed_members:
+    return redirect(url_for("error", message = "帳號已經被註冊"))
+  # 新用戶的狀況
+  else:
+    user_name = request.form.get("name")
+    user_password = request.form.get("password")
+    sql = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
+    values = (user_name, user_username, user_password)
+    cursor.execute(sql, values)
+    
+    db.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
