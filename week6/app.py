197e182288057c85c689c54a6f6cb14871d90a7c
IKaRos <w71800@gmail.com>
2023-08-09 22:36:57 +0800

    complete req.2,3,4

--- week6/app.py
+++ week6/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from db_manufactor import db, cursor
+from flask import Flask, render_template, request, redirect, url_for, session
+from db_manufactor import db, cursor, check_exist
 
 app = Flask(__name__)
+app.secret_key = "secret"
 
 
 @app.route("/")
@@ -10,7 +11,13 @@ def home():
 
 @app.route("/member")
 def member():
-  return render_template("member.html", name = None)
+  is_signed = session.get("signed")
+
+  if is_signed == False or is_signed == None:
+    return redirect("/")
+  
+  name = session.get("name")
+  return render_template("member.html", name = name)
 
 @app.route("/error")
 def error():
@@ -22,21 +29,33 @@ def error():
 def signin():
   user_id = request.form.get("user_id")
   user_password = request.form.get("user_password")
-  # 先比對資料庫中有無此 id，有的話對對看密碼，沒有的話就要求申請帳號
-  # 帳號密碼都對之後，重新導向至 /member，並且用 session 來存取 name
-  return render_template("member.html")
+  # 先比對資料庫中有無此 id，有的話對對看密碼
+  is_exist = check_exist(user_id)
+  
+  if is_exist:
+    sql = "SELECT name, password FROM member WHERE username = %s"
+    cursor.execute(sql, (user_id,))
+    name, pwd = cursor.fetchall()[0]
+    if pwd == user_password:
+      session["name"] = name
+      session["signed"] = True
+      return redirect("/member")
+    else:
+      return redirect(url_for("error", message = "帳號或密碼輸入錯誤"))
+  else:
+    return redirect(url_for("error", message = "帳號或密碼輸入錯誤"))
+  
+  
+  return "test"
 
 # 處理註冊需求
 @app.route("/signup", methods=["POST"])
 def signup():
   user_username = request.form.get("id")
-  sql = "SELECT username from member"
-  cursor.execute(sql)
-  signed_members = cursor.fetchall()
-  signed_members = [ item[0] for item in signed_members ]
+  is_exist = check_exist(user_username)
   
   # 已經被註冊的狀況
-  if user_username in signed_members:
+  if is_exist:
     return redirect(url_for("error", message = "帳號已經被註冊"))
   # 新用戶的狀況
   else:
@@ -49,6 +68,11 @@ def signup():
     db.commit()
     return redirect("/")
 
+@app.route("/signout")
+def signout():
+  session.pop("name", None)
+  session.pop("signed", None)
+  return redirect("/")
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
