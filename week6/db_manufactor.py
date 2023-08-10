d5449052ad1435f1e7d99d937e3ddff5954fbf7e
IKaRos <w71800@gmail.com>
2023-08-10 16:44:12 +0800

    complete Week6

--- week6/db_manufactor.py
+++ week6/db_manufactor.py
@@ -1,5 +1,5 @@
 import mysql.connector as connector
-db = connector.connect(host="localhost", user="root", password="ww171717", database="website")
+db = connector.connect(host="localhost", user="root", password="0000", database="website")
 cursor = db.cursor()
 
 def check_exist(username):
